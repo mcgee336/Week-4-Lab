0d1821969f9bddf111d62d4a9d811f174a2cc8b0
John McGee <mcgee336@morrisville.edu>
2024-09-18 15:44:32 -0400

    Extra Work

--- w4 Project/Assets/Scripts/Spin.cs
+++ w4 Project/Assets/Scripts/Spin.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
     [SerializeField] float rotateSpeed = 450f;
+    
 
-    // Start is called before the first frame update
-    void Start()
+
+// Update is called once per frame
+void Update()
     {
-        
+
+        transform.Rotate(0f, 1 * Time.deltaTime * rotateSpeed, 0f, Space.Self);
+
+     if (gameObject.tag == "Hit")
+        {
+            rotateSpeed = -450f;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-        transform.Rotate(0f, 1 * Time.deltaTime * rotateSpeed, 0f, Space.Self);  
-    }
+
 }
